c4b2826217f43469a6ce8bef6439470d8436221b
jossefloress <54643565+jossefloress@users.noreply.github.com>
2020-08-24 23:20:03 -0500

    Add files via upload

---
+++ Assignment1_AllFiles/Assignment1/Assignment1.cpp
@@ -0,0 +1,26 @@
+/*Assignment1: Outputs a diamond shape made out of asterisks
+Author: Jose Flores
+Date: 6/11/19 */
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	cout << "      *      \n";
+	cout << "     * *     \n";
+	cout << "    * * *    \n";
+	cout << "   * * * *   \n";
+	cout << "  * * * * *  \n";
+	cout << " * * * * * * \n";
+	cout << "  * * * * *  \n";
+	cout << "   * * * *   \n";
+	cout << "    * * *    \n";
+	cout << "     * *     \n";
+	cout << "      *      \n";
+	system("pause");
+	return 0;
+}
+
+
